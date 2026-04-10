5d9ef84b0043c1bbb87714e6e36230d6268db1d8
Shep (Rhea) Pablo <rhea.pko@gmail.com>
2026-04-10 01:13:18 -0400

    Will need QR class's method for scanning

--- FrontDeskStaff.java
+++ FrontDeskStaff.java
@@ -1,16 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class FrontDeskStaff {
-    private int trainerID;
 
-    public void createClass() {
-
+    public void createClass(LocalTime time, int capacity, Trainer trainer, LocalDate day, String type) {
+        new GymClass(time, capacity, trainer, day, type);
     }
 
-    public void editClass() {
-
+    public void editClassTime(GymClass gymClass, LocalTime time) {
+        gymClass.setTime(time);
+    }
+    public void editClassTrainer(GymClass gymClass, Trainer trainer) {
+        gymClass.setTrainer(trainer);
+    }
+    public void editClassCapacity(GymClass gymClass, int capacity) {
+        gymClass.setCapacity(capacity);
     }
 
-    public void scanQR() {
-
-    }
+    //use QR's methods to scan QR
 
 }
